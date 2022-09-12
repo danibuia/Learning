0bbb662f488e3c69529998fce584d5d7ea2a7f2b
Dani Buia <daniel.buia@smartboxdigital.com>
2022-09-12 16:45:52 +0300

    gdfg

--- use-state-function/src/pages/home/Home.jsx
+++ use-state-function/src/pages/home/Home.jsx
@@ -1,48 +1,92 @@
-import React, { useState } from "react";
-import "./Home.css";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { baseUrl } from "./constants/Constants";
 
 
 const Home = () => {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
- 
-    
-    const onLogin = () => {
-
-        axios.post("https://smartboxdigital-academy-be.herokuapp.com/SBD-Academy/api/admins/login", {
-            email: email,
-            password: password,
-           
-        
-
-        }).then((response) => console.log(response)).catch((error) => console.log(error));
-        // window.localStorage.setItem("token", "asasjansjakbsa");
-        // window.localStorage.setItem("User", "User");
-        // window.location.pathname = '/admin';
-    };
-    return (
-        <>
-            <h1>Home (Public)</h1>
-            <form action="">
-                <label htmlFor="Email">Email</label>
-                <input onChange={(event) => setEmail(event.target.value)} type="email" id="email" name="Email" placeholder="Email" />
-
-                <label htmlFor="Password">Password</label>
-                <input onChange={(event) => setPassword(event.target.value)} type="password" id="password" name="Password" placeholder="Password" />
-
-              
-               
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accessToken, setAccessToken] = useState(window.localStorage.getItem("token"))
+  const [data, setData] = useState([])
 
 
-            </form>
-            {/* <input type="submit" value="Sign in" onSubmit={onLogin}/> */}
-            <button name="LogIn" type="button" onClick={onLogin} disabled={false}>
-                Log in heres
-            </button>
-        </>
-    );
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    console.log("value is:", event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+    console.log("value is:", event.target.value);
+  };
+  const onLogin = () => {
+    axios
+      .post(`${baseUrl}/admins/login`, {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log("response ", response.data);
+        window.localStorage.setItem("token", response.data.accessToken);
+        window.location.pathname = "/admin";
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  };
+  const handleMembers = () => {
+    axios
+      .get(`${baseUrl}/admins/get-members/all`, {
+        headers: {
+          Authorization: `token ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        console.log("response ", response.data);
+         setAccessToken(response.data.accessToken)
+        setData(response.data.members)
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  };
+  useEffect(() => {
+    handleMembers();
+  }, []);
+  return (
+    <>
+      <h2>Home (Public). Hi ,{data[8]?.firstName } !</h2>
+      <div>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <label htmlFor="Email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleEmail}
+          />
+          <label htmlFor="Password">Password</label>
+          <input
+            type="passwod"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={handlePassword}
+          />
+        </form>
+        <button type="button" onClick={onLogin} disabled={false}>
+          Sign In
+        </button>
+      </div>
+    </>
+  );
 };
 export default Home;
---
+++ use-state-function/src/pages/home/constants/Constants.jsx
@@ -0,0 +1,2 @@
+
+export const baseUrl = "https://smartboxdigital-academy-be.herokuapp.com/SBD-Academy/api"
